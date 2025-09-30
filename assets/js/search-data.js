aacde9b1220de9e130d0c8d37b51330106947a66
nurimertvural <n.mertv@gmail.com>
2025-09-30 17:01:20 +0000

    Deploying to gh-pages from @ nurimertvural/nurimertvural.github.io@7854f204f2590a0f5bbe684969ca7045e1f705d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "denotes alphabetical ordering or equal contribution. For complete list see my Google Scholar.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
