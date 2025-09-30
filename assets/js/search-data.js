25a7119bdcaaea36b003ef774534145971c2468b
nurimertvural <n.mertv@gmail.com>
2025-09-30 02:39:36 +0000

    Deploying to gh-pages from @ nurimertvural/nurimertvural.github.io@5918d6414623814959d735509849f106b04bf992 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,26 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/assets/pdf/nmv_resume.pdf";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
