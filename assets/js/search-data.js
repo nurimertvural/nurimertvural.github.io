44f4ba5d3d1b500cf37226d5d1b349659385558e
nurimertvural <n.mertv@gmail.com>
2025-09-30 02:29:35 +0000

    Deploying to gh-pages from @ nurimertvural/nurimertvural.github.io@0ccb59f2907c6b00fa2736a42adedc491b566e3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,10 +39,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/nmv_resume.pdf";
           },
         },{id: "nav-teaching",
           title: "teaching",
